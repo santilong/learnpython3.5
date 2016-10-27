a456060e35f1068e737e6874d3827e2c0de6a42e
santi <sandiegoit99@gmail.com>
2016-10-27 18:13:59 +0800

    modify the script of iftest.

--- iftest.py
+++ iftest.py
@@ -1,9 +1,11 @@
 #-*- coding: utf-8 -*-
 #age = 20
-age = input('请输入你的年龄：')
-age = int(age)
-if age <21:
+age = input('请输入你的年龄：')    
+age = int(age) 		###input返回的数据类型是str，str不能和整数比较，必须先把str转换成整数
+if age <= 14:
     print('滚吧，小屁孩！')
+elif age > 14 and age < 20:
+    print('青少年！')
 else:
-    print('老年人！')
+    print("青年人！")
 
